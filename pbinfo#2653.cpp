3e7a30daa8796d6ad4133f9ab51bd3d8d3d608c0
Cioaca <cotet.geo.cris.2007@gmail.com>
2025-07-26 18:17:30 +0300

    modified:   pbinfo#2653.cpp     solutia 1 de testat n = 10 ^ 3 si x = 10 ^ 9     de sters cometariile cout

--- pbinfo#2653.cpp
+++ pbinfo#2653.cpp
@@ -1,44 +1,60 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
-// n = puterea radicalului
-// x = nr respectiv
 
 long long nroot(int n, long long x)
 {
-    if( x == 0 ) return 0;
-    else if( x == 1 ) return 1;
-    else if( n > x) return 0;
-    else if( x < 0) x = x - x * 2;
-    else if( n == 1 ) return x;
+    long long  ans = 0; 
+    if( x == 0 ) ans = 0;
+    else if( x == 1 ) ans = 1;
+    else if( n > x) ans = 1;
+    else if( x < 0) x = 0 - x;
+    else if( n == 1 ) ans = x;
     // taking out special cases.
-    // start of binary search.
-    int st = 1;
-    int dr = x;
-    int mij , i;
-    int ans = 0; 
-    while (dr - st > 1)
+    else 
     {
-      long long aux = mij;
-      mij = (st + dr) / 2;
-      if(mij * mij > x) // if the middle's pow to 2 is greater than x we move downwards.
-        dr = mij - 1;
-      // add a new if statement if the mij * mij gets past of long long range.
-      else if(mij * mij <= x) // we found a number which might be good.
-        {
-          for( i = 1; i < n; i++) // we check if the middle times n itself goes close or equal to x.
-          aux = aux * aux;
-          if(aux == x) return mij; 
-          // now we try to get as much as possible to the number x.
-          else if ( aux < x )
+      // start of binary search.
+      int st = 1;
+      int dr = x/2;
+      int mij , i;
+      while (dr - st > 0)
+      {
+        mij = (st + dr) / 2;
+        long long aux = mij;
+        if(mij * mij > x) // if the middle's pow to 2 is greater than x we move downwards.
           {
-            ans = aux;
+            cout << "ma duc in stanga 1 " << st << ' ' << mij << ' ' << dr <<' ' << aux << endl;
+            dr = mij - 1;
+          }
+        // add a new if statement if the mij * mij gets past of long long range.
+        else // (mij * mij <=x)  we found a number which might be good.
+        {
+          for( i = 1; i < n && aux <= x; i++) // we check if the middle times n itself goes close or equal to x.
+          {
+            aux = aux * aux;
+            cout << " dupa inmultire: " << aux << endl;
+          }
+           cout << " dupa inmultire: " << aux << endl;
+          if(aux == x)
+          {
+            cout << "este egal " << st << ' ' << mij << ' ' << dr <<' ' << aux << endl;
+            dr = st;
+          } 
+          // now we try to get as much as possible to the number x.
+          else if (aux < x)
+          {
+            cout << "ma duc in dreapta " << st << ' ' << mij << ' ' << dr << ' ' << aux << endl;
             st = mij + 1;
           }
           else
-            dr = mij - 1; 
+          {
+            cout << "ma duc in stanga 2 " << st << ' ' << mij << ' ' << dr <<' ' << aux << endl;
+            dr = mij - 1;
+          } 
         }
+      }
+      ans = st;
     }
+    
     return ans;
 }
 
@@ -46,8 +62,7 @@ int main()
 {
   int n;
   long long x;
-  cin >> n;
-  cin >> x;
+  cin >> n >> x;
   cout << nroot(n , x);
 
 }
@@ -56,5 +71,3 @@ int main()
 // perfect square which is smaller than x 
 
 // pbinfo #2653 nroot
-
-// change test2
