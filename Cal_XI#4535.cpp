3e8f3de285cb3952e83eb636a99bdcf714d96e3f
dimitriedmr <panainte.dimitrie@gmail.com>
2025-11-03 07:52:48 +0200

    vizitat in loc de sol

--- Cal_XI#4535.cpp
+++ Cal_XI#4535.cpp
@@ -1,133 +1,93 @@
-#include <iostream>
-#include <fstream>
-#include <limits>
+#include <bits/stdc++.h>
 using namespace std;
-ifstream fin("cal_xi.in");
-ofstream fout("cal_xi.out");
 
-
-// ideea de rezolvare:
-/*
-    numar cati pioni am pe tabla.
-    imi iau o valoare auxiliara la nr pionilor.
-    atunci cand nr_pioni_aux = 0 inseamna ca am gasit o solutie.
-    problema apare atunci cand:
-    dupa ce am gasit o solutie trebuie sa imi reinitializez nr_pioni_aux. 
-    Rezolvarea acestui impediment se face atunci cand:
-    sol[k].l ajunge la linia de unde am plecat (sol[k].l == is) si cand sol[k].c ajunge la coloana de unde am plecat (sol[k].c == js)
-    Vom scrie acest lucru in else de la if(exista(k)). 
-    De ce?
-    Pai cand sol[k].l && sol[k].c ajung la is, js?
-    Atunci cand se scade valoarea lui k pana la k = 2.
-
-*/
-
+#define N_MAX 10
+#define M_MAX 10
+#define NM_MAX (N_MAX * M_MAX)
 
 struct poz
 {
     int l, c;
-}sol[100];
+} sol[NM_MAX + 1];
 
-
-
-int n, m, k, exista_solutie;    
-int nr_pioni , nr_minim_pasi = 2147483647, nr_solutii;
-int is, js;
-int d[100], nr_pioni_ramasi[100], dl[] = {-2, -2, -1, 1, 2, 2, 1, -1}, dc[] = {-1 , 1, 2, 2, 1, -1, -2, -2};
-int a[11][11];
+int n, m, k;
+int d[NM_MAX + 1], dl[] = {-2, -2, -1, 1, 2, 2, 1, -1}, dc[] = {-1, 1, 2, 2, 1, -1, -2, -2};
+int nr_pioni_ramasi, nr_pioni, nr_minim_pasi = INT32_MAX, nr_solutii;
+int a[N_MAX + 1][M_MAX + 1];
+int vizitat[N_MAX + 1][M_MAX + 1]; // marcăm pozițiile vizitate
 
 void init(int k)
 {
     d[k] = -1;
-    nr_pioni_ramasi[k] =  nr_pioni_ramasi[k - 1];
 }
 
 int exista_directie(int k)
 {
-    int ans;
-    if(d[k] < 7)
-        ans = 1;
-    else
-        ans = 0;
-    return ans;
+    return (d[k] < 7);
 }
 
 void valpos(int k)
 {
     sol[k].l = sol[k - 1].l + dl[d[k]];
-    
     sol[k].c = sol[k - 1].c + dc[d[k]];
 }
 
 int cont(int k)
 {
     int ans = 1;
-    if(a[sol[k].l][sol[k].c] == 3)
+    if (a[sol[k].l][sol[k].c] == 3)
+        ans = 0;
+    else if (sol[k].l < 1 || sol[k].l > n || sol[k].c < 1 || sol[k].c > m)
+        ans = 0;
+    else if (vizitat[sol[k].l][sol[k].c]) // deja vizitat
         ans = 0;
 
-    if(sol[k].l < 1 || sol[k].l > n || sol[k].c < 1 || sol[k].c > m)
-        ans = 0;
-
-    for(int i = 1; i <= k - 1; i++)
-        if(sol[i].l == sol[k].l && sol[i].c == sol[k].c)
-            ans = 0;
-    
     return ans;
 }
 
 int solutie(int k)
 {
-    int ans;
-    if(a[sol[k].l][sol[k].c] == 1)
-        nr_pioni_ramasi[k]--;
-    if(nr_pioni_ramasi[k] == 0)
-        ans = 1;
-    else
-        ans = 0;
-    return ans;
-}
-
-/*
-void tipar(int k)
-{
-    for(int i = 1; i <= n; i++)
-    {
-        for(int j = 1; j <= m; j++)
-            cout << a[i][j] << ' ';
-        cout << endl;
-    }
-   cout << endl;
-}
-*/
-
-void afisare_raspunsuri(int k)
-{
-    nr_solutii++;
-    if(k < nr_minim_pasi)
-        nr_minim_pasi = k;
-    exista_solutie = 1;
-    
+    return (nr_pioni_ramasi == 0);
 }
 
 void bkt()
 {
-    k = 2;
+    k++; // urmatoarea pozitie
     init(k);
-    while(k > 1)
+
+    while (k > 1)
     {
-        if(exista_directie(k))
+        if (exista_directie(k))
         {
-            d[k] = d[k] + 1;
+            // verific directia urmatoare
+            d[k]++;
             valpos(k);
-            if(cont(k))
+            if (cont(k))
             {
-                if(solutie(k))
+                // pozitie valida
+                // actualizare pioni ramasi
+                if (a[sol[k].l][sol[k].c] == 1)
+                    nr_pioni_ramasi--;
+
+                if (solutie(k))
                 {
-                    afisare_raspunsuri(k);
+                    nr_solutii++;
+                    if (k < nr_minim_pasi)
+                        nr_minim_pasi = k;
+                    // dupa ce am gasit o solutie:
+                    //  trebuie restaurat:
+                    // 1. pionii ramasi
+                    if (a[sol[k].l][sol[k].c] == 1)
+                        nr_pioni_ramasi++;
+                    // 2. demarcăm poziția
+                    vizitat[sol[k].l][sol[k].c] = 0;
                 }
                 else
                 {
-                    
+                    // cu toate ca este o pozitie valida, nu este finalizata gasirea unei solutii
+                    // marchez pozitia k ca fiind vizitata
+                    vizitat[sol[k].l][sol[k].c] = 1;
+                    //  si trec la urmatoarea pozitie
                     k++;
                     init(k);
                 }
@@ -135,49 +95,48 @@ void bkt()
         }
         else
         {
+            // nu mai am directii de verificat
+            //  si revin la pozitia anterioara
             k--;
-            nr_pioni_ramasi[k] =  nr_pioni_ramasi[k - 1];
-        }  
-        // aici verific daca am ajuns la pozitia de start
-        // voi reinitializa nr_pioni_aux = nr_pioni
-        
-                     
+            //  trebuie restaurat:
+            // 1. pionii ramasi
+            if (a[sol[k].l][sol[k].c] == 1)
+                nr_pioni_ramasi++;
+            // 2. demarcăm poziția
+            vizitat[sol[k].l][sol[k].c] = 0;
+        }
     }
-    
 }
-
-
 
 int main()
 {
+    k = 1; // pozitia de start
+    ifstream fin("cal_xi.in");
+    if (fin) // read from console if file doesn't exist
+        cin.rdbuf(fin.rdbuf());
+    ofstream fout("cal_xi.out");
     // citire
-    fin >> n >> m;
-    for(int i = 1; i <= n; i++)
-        for(int j = 1; j <= m; j++)
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++)
+        {
+            cin >> a[i][j];
+            if (a[i][j] == 1)
+                nr_pioni++;
+            if (a[i][j] == 2)
             {
-                fin >> a[i][j];
-                if(a[i][j] == 1)
-                    nr_pioni++;
-                if(a[i][j] == 2)
-                {
-                    is = i;
-                    js = j;
-                }
+                sol[k].l = i;
+                sol[k].c = j;
             }
-    nr_pioni_ramasi[1] = nr_pioni;
-    sol[1].l = is;
-    sol[1].c = js;
+        }
+    vizitat[sol[k].l][sol[k].c] = 1; // marcăm poziția de start ca vizitată
+    nr_pioni_ramasi = nr_pioni;
     // gasire solutii
     bkt();
-    if(exista_solutie == 0)
-        fout << "IMPOSIBIL";
+    // afisare
+    if (nr_solutii <= 0)
+        cout << "IMPOSIBIL";
     else
-        fout << nr_solutii << ' ' <<  nr_minim_pasi - 1;
-
+        cout << nr_solutii << ' ' << nr_minim_pasi - 1; // pornesc de la 0
+    cout << endl;
 }
-/*
-    // aici verific daca am ajuns la pozitia de start
-        // voi reinitializa nr_pioni_aux = nr_pioni
-        if(sol[k].l == is && sol[k].c == js)
-                nr_pioni_aux = nr_pioni; 
-*/
