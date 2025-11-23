983745daf4e242760e3cbc103aebf74cf22c65a2
dimitriedmr <panainte.dimitrie@gmail.com>
2025-11-23 18:05:15 +0200

    90 pct

--- METODA_GREEDY/easy_scc#2683.cpp
+++ METODA_GREEDY/easy_scc#2683.cpp
@@ -3,40 +3,27 @@ using namespace std;
 
 int main()
 {
-    int n, curent, curent_urmator = 0, ok = 0, nr_siruri = 0, primul_curent_urmator = 0;
-    int sir[1001];
+    int n = 0, curent = -1, ok = 0, nr_siruri = -1;
+    int sir[1001] = {};
+
     cin >> n;
     for (int i = 1; i <= n; i++)
         cin >> sir[i];
-    curent = sir[1];
-    sir[1] = -1;
+
     while (ok == 0)
     {
+        ++nr_siruri;
         ok = 1;
-        for (int i = 2; i <= n; i++)
+        for (int i = 1; i <= n; i++)
         {
-            if (curent < sir[i] && sir[i] > -1)
+            if (sir[i] > -1 && curent <= sir[i])
             {
                 curent = sir[i];
                 sir[i] = -1;
                 ok = 0;
             }
-            else
-            {
-                if (primul_curent_urmator == 0 && sir[i] > -1)
-                {
-                    curent_urmator = sir[i];
-                    sir[i] = -1;
-                    primul_curent_urmator++;
-                }
-            }
         }
-        if (primul_curent_urmator > 0 || ok == 0)
-        {
-            nr_siruri++;
-            primul_curent_urmator = 0;
-            curent = curent_urmator;
-        }
+        curent = -1;
     }
     cout << nr_siruri;
 }
